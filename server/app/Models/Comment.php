ae1e8be4a6059328feea244f4d13e58f633c98ea
Ahmed-Foullane <ahmedfoullane@gmail.com>
2025-04-21 16:32:47 +0100

    Implement Eloquent models with relationships and properties

---
+++ server/app/Models/Comment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'votes',
+        'is_accepted',
+        'question_id',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'is_accepted' => 'boolean',
+    ];
+
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes(){
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+
+}
